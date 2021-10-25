31c35f131ce730aa47c3df6075123dba8bc46fc6
Swetank Subham <swtnk.sbhm.r@gmail.com>
2021-10-25 15:44:16 +0530

    Added 2nd solution for problem 1.

--- p1.py
+++ p1.py
@@ -17,7 +17,7 @@ GHIJ
 ... upto Z
 """
 
-# SOLUTION 1
+# START: SOLUTION 1
 def get_break_point():
     next_break_point, incrementor = 0, 1
     while True:
@@ -25,10 +25,10 @@ def get_break_point():
         incrementor += 1
         next_break_point += incrementor
 
-def print_pattern():
+def print_pattern_solution_1():
     break_point_index = get_break_point()
     break_point = None
-    for index, alphabet in enumerate([chr(ascii) for ascii in range(65, 91)]):
+    for index, alphabet in enumerate([chr(ascii_value) for ascii_value in range(65, 91)]):
         if break_point is None:
             break_point = next(break_point_index)
         if index != break_point:
@@ -39,4 +39,21 @@ def print_pattern():
         print(alphabet, end=end)
     print()
 
-print_pattern()
+print_pattern_solution_1()
+# END: SOLUTION 1
+
+
+# START: SOLUTION 2
+def print_pattern_solution_2():
+    ascii_value = 65
+    for i in range(7):
+        for j in range(i+1):
+            alphabet = chr(ascii_value)
+            print(alphabet, end='')
+            ascii_value += 1
+            if ascii_value > 90:
+                break
+        print()
+
+print_pattern_solution_2()
+# END: SOLUTION 2
